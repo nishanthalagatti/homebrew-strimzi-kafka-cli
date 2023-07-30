50c00c720098732fa5334aab6bdbdfd923b7fb2d
Aykut M. Bulgu <aykut@systemcraftsman.com>
2023-07-31 01:57:14 +0300

    Update strimzi-kafka-cli.rb

--- strimzi-kafka-cli.rb
+++ strimzi-kafka-cli.rb
@@ -3,7 +3,7 @@ class StrimziKafkaCli < Formula
 
   desc "Command Line Interface for Strimzi Kafka Operator"
   homepage "https://github.com/systemcraftsman/strimzi-kafka-cli"
-  version "0.1.0a66"
+  version "0.1.0a67"
   url "https://files.pythonhosted.org/packages/source/s/strimzi-kafka-cli/strimzi-kafka-cli-"+version+".tar.gz"
   license "Apache-2.0"
 
