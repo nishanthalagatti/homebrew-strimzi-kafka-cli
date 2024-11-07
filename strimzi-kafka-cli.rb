36e384c2475cd8790f161dc1e35cd361e3ac8329
Aykut M. Bulgu <aykut@systemcraftsman.com>
2024-11-07 12:42:37 +0300

    Update strimzi-kafka-cli.rb

--- strimzi-kafka-cli.rb
+++ strimzi-kafka-cli.rb
@@ -3,14 +3,14 @@ class StrimziKafkaCli < Formula
 
   desc "Command Line Interface for Strimzi Kafka Operator"
   homepage "https://github.com/systemcraftsman/strimzi-kafka-cli"
-  version "0.1.0a78"
+  version "0.1.0a79"
   url "https://files.pythonhosted.org/packages/source/s/strimzi-kafka-cli/strimzi-kafka-cli-"+version+".tar.gz"
   license "Apache-2.0"
 
-  depends_on ["python@3.11", "python@3.10", "python@3.9", "python@3.8"]
+  depends_on ["python@3.13", "python@3.12", "python@3.11", "python@3.10", "python@3.9"]
 
   def install
-    venv = virtualenv_create(libexec, python="python3.11", without_pip: false)
+    venv = virtualenv_create(libexec, python="python3.12", without_pip: false)
 
     @venv_root = venv.instance_variable_get(:@venv_root)
     @formula = venv.instance_variable_get(:@formula)
